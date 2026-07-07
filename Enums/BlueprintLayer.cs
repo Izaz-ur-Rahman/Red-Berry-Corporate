1b2ee30aea8da91915bd82d6030907e64a0c1b0a
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-07-07 13:10:00 +0500

    feat(blueprint): implement blueprint submission entity and database foundation

---
+++ Enums/BlueprintLayer.cs
@@ -0,0 +1,13 @@
+﻿namespace RedBerryCorporate.Enums
+{
+    public enum BlueprintLayer
+    {
+        Corporate = 1,
+
+        Financial = 2,
+
+        Market = 3,
+
+        Legacy = 4
+    }
+}
