bd4404ba40047dd26d56544815f7c0907ffc5120
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-07-14 16:14:39 +0500

    feat(blog): implement repository support for publish archive restore scheduled blogs and soft delete

--- Interfaces/Blog/IBlogRepository.cs
+++ Interfaces/Blog/IBlogRepository.cs
@@ -3,22 +3,34 @@ using RedBerryCorporate.Models;
 
 namespace RedBerryCorporate.Interfaces.Blog
 {
+    using BlogEntity = RedBerryCorporate.Models.Blog;
     public interface IBlogRepository
     {
-        Task<Models.Blog> AddAsync(Models.Blog blog);
 
-        Task<Models.Blog?> UpdateAsync(Models.Blog blog);
+        Task<BlogEntity> AddAsync(BlogEntity blog);
 
-        Task<bool> DeleteAsync(int id);
+        Task<BlogEntity> UpdateAsync(BlogEntity blog);
 
-        Task<Models.Blog?> GetByIdAsync(int id);
+        Task<bool> DeleteAsync(int id, int deletedByUserId);
 
-        Task<Models.Blog?> GetBySlugAsync(string slug);
+        Task<BlogEntity?> GetByIdAsync(int id);
 
-        Task<(List<RedBerryCorporate.Models.Blog> Blogs, int TotalCount)> GetAllAsync(BlogQueryDto query);
-        Task<List<Models.Blog>> GetPublishedAsync();
+        Task<BlogEntity?> GetBySlugAsync(string slug);
+
+        Task<(List<BlogEntity> Blogs, int TotalCount)> GetAllAsync(BlogQueryDto query);
+
+        Task<List<BlogEntity>> GetPublishedAsync();
+
+        Task<List<BlogEntity>> GetScheduledBlogsAsync();
+
+        Task<bool> PublishAsync(int id, int publishedByUserId);
+
+        Task<bool> ArchiveAsync(int id, int updatedByUserId);
+
+        Task<bool> RestoreAsync(int id, int updatedByUserId);
 
         Task<bool> IncrementOpenCountAsync(int id);
+
         Task<bool> SlugExistsAsync(string slug, int? ignoreId = null);
     }
 }
