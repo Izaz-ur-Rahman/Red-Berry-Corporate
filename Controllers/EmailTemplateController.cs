5961b2c1334118c8eb11704d97a0fd0e57a7d18f
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-07-18 12:10:57 +0500

    feat(email-template): add EmailTemplate entity and database table

---
+++ Controllers/EmailTemplateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedBerryCorporate.Controllers
+{
+    public class EmailTemplateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/EmailTemplate.cs
@@ -0,0 +1,25 @@
+﻿namespace RedBerryCorporate.Models
+{
+    public class EmailTemplate
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string TemplateKey { get; set; } = null!;
+
+        public string Subject { get; set; } = null!;
+
+        public string BodyHtml { get; set; } = null!;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; }
+
+        public int? CreatedByUserId { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public int? UpdatedByUserId { get; set; }
+    }
+}
