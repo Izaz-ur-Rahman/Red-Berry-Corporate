2d76ea72d56c41a1dde0819046ffafe8bb42eb26
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-07-23 12:23:14 +0500

    add interface for repository

---
+++ Interfaces/Notification/INotificationRepository.cs
@@ -0,0 +1,21 @@
+﻿using RedBerryCorporate.DTOs.Notification;
+using RedBerryCorporate.Models;
+
+namespace RedBerryCorporate.Interfaces.Notification
+{
+    public interface INotificationRepository
+    {
+        Task<Notification> AddAsync(Notification notification);
+
+        Task<(List<Notification> Notifications, int TotalCount)>
+            GetAllAsync(NotificationQueryDto query);
+
+        Task<Notification?> GetByIdAsync(int id);
+
+        Task<int> GetUnreadCountAsync();
+
+        Task<bool> MarkAsReadAsync(int id);
+
+        Task<int> MarkAllAsReadAsync();
+    }
+}
