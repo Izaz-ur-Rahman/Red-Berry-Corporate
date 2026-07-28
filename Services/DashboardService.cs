3c680044168d0f91806a0ec2ab81479bf1d5907d
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-07-28 11:19:20 +0500

    feat(dashboard): implement dashboard service

---
+++ Services/DashboardService.cs
@@ -0,0 +1,20 @@
+﻿using RedBerryCorporate.DTOs.Dashboard;
+using RedBerryCorporate.Interfaces.Dashboard;
+
+namespace RedBerryCorporate.Services
+{
+    public class DashboardService : IDashboardService
+    {
+        private readonly IDashboardRepository _repository;
+
+        public DashboardService(IDashboardRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<DashboardResponseDto> GetDashboardAsync()
+        {
+            return await _repository.GetDashboardAsync();
+        }
+    }
+}
