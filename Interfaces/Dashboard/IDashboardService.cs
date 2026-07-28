05f87b21905015077ee26e6b44f6b599c1e87ff8
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-07-28 11:16:45 +0500

    feat(dashboard): add dashboard service interface

---
+++ Interfaces/Dashboard/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using RedBerryCorporate.DTOs.Dashboard;
+
+namespace RedBerryCorporate.Interfaces.Dashboard
+{
+    public interface IDashboardService
+    {
+        Task<DashboardResponseDto> GetDashboardAsync();
+    }
+}
