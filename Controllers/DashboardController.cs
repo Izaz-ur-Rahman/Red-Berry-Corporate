d662f19c7e3dc32334ffc05b85dac0cb8755a887
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-07-28 11:22:19 +0500

    feat(dashboard): add dashboard statistics endpoint

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedBerryApi.Controllers;
+using RedBerryCorporate.DTOs.Common;
+using RedBerryCorporate.DTOs.Dashboard;
+using RedBerryCorporate.Helpers;
+using RedBerryCorporate.Interfaces.Dashboard;
+
+namespace RedBerryCorporate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : BaseApiController
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(
+            IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet("Stats")]
+        public async Task<IActionResult> Stats()
+        {
+            var result =
+                await _dashboardService.GetDashboardAsync();
+
+            return Ok(new ApiResponse<DashboardResponseDto>
+            {
+                Success = true,
+                Message = "Dashboard statistics retrieved successfully.",
+                Data = result
+            });
+        }
+    }
+}
