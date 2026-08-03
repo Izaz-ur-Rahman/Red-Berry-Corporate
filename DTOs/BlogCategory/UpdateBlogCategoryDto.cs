ab3bdd81ffe62d67176265f47fc3d7e77afa36ea
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-08-03 10:46:15 +0500

    feat(blog-category): add category DTOs

---
+++ DTOs/BlogCategory/UpdateBlogCategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedBerryCorporate.DTOs.BlogCategory
+{
+    public class UpdateBlogCategoryDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
