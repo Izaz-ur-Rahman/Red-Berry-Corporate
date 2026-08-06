0b22e95578c7736b5ed9771b8c13ffe20ccca6a5
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-08-06 15:35:35 +0500

    feat(blog): migrate legacy categories to category relationships

---
+++ Migrations/20260806101617_MigrateBlogCategories.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedBerryCorporate.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateBlogCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+    UPDATE Blogs
+    SET CategoryId = BlogCategories.Id
+    FROM Blogs
+    INNER JOIN BlogCategories
+        ON LOWER(LTRIM(RTRIM(Blogs.Category))) =
+           LOWER(LTRIM(RTRIM(BlogCategories.Name)))
+    WHERE Blogs.CategoryId IS NULL
+      AND Blogs.Category IS NOT NULL;
+");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
